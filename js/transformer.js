dd438783b2e88b790758d9ab908c542efe2910e6
Gonçalo Aroeira Gonçalves <goncalo.b.aroeira@tecnico.ulisboa.pt>
2023-05-15 09:36:21 +0100

    side camera

--- js/transformer.js
+++ js/transformer.js
@@ -2,7 +2,7 @@
 
 var scene, renderer;
 
-var frontalCamera, sideCamera, aboveCamera, isometricOrthogonalCamera, isometricPerspectiveCamera
+var frontalCamera, sideCamera, aboveCamera, isometricOrthogonalCamera, isometricPerspectiveCamera, activeCamera;
 
 var geometry, material, mesh;
 
@@ -12,8 +12,8 @@ function onResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     if (window.innerHeight > 0 && window.innerWidth > 0) {
-        frontalCamera.aspect = window.innerWidth / window.innerHeight;
-        frontalCamera.updateProjectionMatrix();
+        activeCamera.aspect = window.innerWidth / window.innerHeight;
+        activeCamera.updateProjectionMatrix();
     }
 
 }
@@ -22,6 +22,12 @@ function onKeyDown(e) {
     'use strict';
 
     switch (e.keyCode) {
+        case 49: 
+            activeCamera = frontalCamera;
+            break;
+        case 50:
+            activeCamera = sideCamera;
+            break;
     }
 }
 
@@ -37,8 +43,21 @@ function createCamera() {
         1,          // Near
         1000        // Far
       );
+
+    sideCamera = new THREE.OrthographicCamera(
+        width / -2, // Left
+        width / 2,  // Right
+        height / 2, // Top
+        height / -2, // Bottom
+        1,          // Near
+        1000        // Far
+      );
+
     frontalCamera.position.set(0, 0, 100)
     frontalCamera.lookAt(scene.position);
+
+    sideCamera.position.set(100,0,0)
+    sideCamera.lookAt(scene.position);
 }
 
 function createScene() {
@@ -57,7 +76,7 @@ function createScene() {
 
 function render() {
     'use strict';
-    renderer.render(scene, frontalCamera);
+    renderer.render(scene, activeCamera);
 }
 
 function animate() {
@@ -76,6 +95,8 @@ function init() {
 
     createScene();
     createCamera();
+
+    activeCamera = frontalCamera;
 
     render();
 
