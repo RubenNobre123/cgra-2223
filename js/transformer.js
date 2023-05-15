7129298508f0839901bab7eba1501c1ecb0e031e
Ruben Nobre <rubennobre2001@tecnico.ulisboa.pt>
2023-05-15 08:12:58 +0100

    first commit

---
+++ js/transformer.js
@@ -0,0 +1,78 @@
+/*global THREE, requestAnimationFrame, console*/
+
+var camera, scene, renderer;
+
+var geometry, material, mesh;
+
+function onResize() {
+    'use strict';
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+
+}
+
+function onKeyDown(e) {
+    'use strict';
+
+    switch (e.keyCode) {
+    }
+}
+
+function createCamera() {
+    'use strict';
+    camera = new THREE.PerspectiveCamera(70,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera.position.x = 50;
+    camera.position.y = 50;
+    camera.position.z = 50;
+    camera.lookAt(scene.position);
+}
+
+function createScene() {
+    'use strict';
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xeeeeee );
+
+    const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+    const points = [];
+    points.push( new THREE.Vector3( - 10, 0, 0 ) );
+    points.push( new THREE.Vector3( 0, 10, 0 ) );
+    points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+    const geometry = new THREE.BufferGeometry().setFromPoints( points );
+    const line = new THREE.Line( geometry, material );
+    scene.add(line);
+
+    scene.add(new THREE.AxisHelper(10));
+
+}
+
+function render() {
+    'use strict';
+    renderer.render(scene, camera);
+}
+
+function init() {
+    'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    createScene();
+    createCamera();
+
+    render();
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("resize", onResize);
+}
